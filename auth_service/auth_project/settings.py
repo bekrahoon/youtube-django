6633ea1abf47b8cba380a5f76250c01b60a25e35
Bekrahoon Umurjanov <bekrahoon12@gmail.com>
2024-11-18 15:25:13 +0600

    Fix: auth_service/auth_project/settings.py

--- auth_service/auth_project/settings.py
+++ auth_service/auth_project/settings.py
@@ -16,8 +16,6 @@ from datetime import timedelta
 from pathlib import Path
 import os
 
-from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
