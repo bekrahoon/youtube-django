b4145f89d7d21b146d0bbf66229793f493d05800
Bekrahun <146942992+bekrahoon@users.noreply.github.com>
2025-07-09 14:32:23 +0600

    Update profile.py

--- auth_service/views/profile.py
+++ auth_service/views/profile.py
@@ -17,7 +17,9 @@ class UserProfileDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["user"] = self.request.user
-        return cont
+        return context
+
+
 class UserProfileUpdateView(UpdateView):
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
     authentication_classes = [JWTAuthentication]
